46eb0ef982b3b1a6cf0c5a68d2a907ca7bef60ee
Tagoon Prappre <tagoon.p@psu.ac.th>
2021-06-09 16:36:03 +0700

    202106091519

--- app/api/test.php
+++ app/api/test.php
@@ -1,6 +1,7 @@
 <?php 
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Header: *");
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: *");
 $return = array();
 $return['a'] = '123';
 $return['b'] = 'asd';

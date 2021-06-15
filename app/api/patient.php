69461abd2838e04d94825fca87143148189a7d08
Tagoon Prappre <tagoon.p@psu.ac.th>
2021-06-15 14:32:10 +0700

    202106151037

---
+++ app/api/patient.php
@@ -0,0 +1,41 @@
+<?php 
+session_start();
+require('../../../database_config/thvot/config.inc.php');
+require('../config/configuration.php');
+require('../config/database.php'); 
+
+$db = new Database();
+$conn = $db->conn();
+
+if(!isset($_GET['stage'])){ $db->close(); header('Location: ../404?stage=001'); die(); }
+$stage = mysqli_real_escape_string($conn, $_GET['stage']);
+$return = array();
+
+if($stage == 'list'){
+    if(
+        (!isset($_GET['uid'])) ||
+        (!isset($_GET['hcode']))
+    ){
+        $return['status'] = 'Fail (x101)';
+        echo json_encode($return);
+        $db->close(); 
+        die();
+    }
+
+    $uid = mysqli_real_escape_string($conn, $_GET['uid']);
+    $hcode = mysqli_real_escape_string($conn, $_GET['hcode']);
+
+    $strSQL = "SELECT * FROM vot2_account a INNER JOIN vot2_userinfo b ON a.uid = b.info_uid 
+              WHERE a.hcode = '$hcode' AND b.info_use AND a.delete_status = '0'";
+    $res = $db->fetch($strSQL,true,false);
+    if(($res) && ($res['status'])){
+        $return['status'] = 'Success';
+        $return['data'] = $res['data'];
+    }else{
+        $return['status'] = 'Fail (x102)';
+    }
+    echo json_encode($return);
+    $db->close(); 
+    die();
+
+}

6d7d71b5a9ed6551ba8a23a3c397a7531fa7a23e
Tagoon Prappre <tagoon.p@psu.ac.th>
2021-06-16 10:26:39 +0700

    202106160829

--- app/api/patient.php
+++ app/api/patient.php
@@ -115,8 +115,9 @@ if($stage == 'patient_info'){
     $uid = mysqli_real_escape_string($conn, $_GET['uid']);
     $patient_id = mysqli_real_escape_string($conn, $_GET['patient_id']);
 
-    $strSQL = "SELECT a.*, b.*, a.ID user_id 
+    $strSQL = "SELECT a.*, b.*, a.ID user_id, c.hosname
                FROM vot2_account a INNER JOIN vot2_userinfo b ON a.uid = b.info_uid 
+               INNER JOIN vot2_chospital c ON a.hcode = c.hoscode
                WHERE a.username = '$patient_id' 
                AND a.delete_status = '0' 
                AND b.info_use = '1'

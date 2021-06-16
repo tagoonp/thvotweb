0bf4081357c5ee4cefd5834e1ad224e66f4033a0
Tagoon Prappre <tagoon.p@psu.ac.th>
2021-06-16 16:18:14 +0700

    202106160842

--- app/api/notification.php
+++ app/api/notification.php
@@ -36,3 +36,78 @@ if($stage == 'admin_notinumber'){
     $db->close(); 
     die();
 }
+
+if($stage == 'list_noti'){
+    if(
+        (!isset($_GET['uid'])) ||
+        (!isset($_GET['role'])) ||
+        (!isset($_GET['hcode'])) ||
+        (!isset($_GET['page'])) ||
+        (!isset($_GET['limie']))
+    ){
+        $return['status'] = 'Fail (x101)';
+        echo json_encode($return);
+        $db->close(); 
+        die();
+    }
+
+    $uid = mysqli_real_escape_string($conn, $_GET['uid']);
+    $role = mysqli_real_escape_string($conn, $_GET['role']);
+    $hcode = mysqli_real_escape_string($conn, $_GET['hcode']);
+    $page = mysqli_real_escape_string($conn, $_GET['page']);
+    $limit = mysqli_real_escape_string($conn, $_GET['limit']);
+
+    if($role == 'admin'){
+        $strSQL = "SELECT COUNT(noti_id) cnt FROM vot2_notification 
+              WHERE 
+              noti_allow_admin = '1' 
+              AND noti_view = '0' 
+              AND noti_type = 'workprocess'
+              ";
+        $res = $db->fetch($strSQL, true, false);
+        if(($res) && ($res['status'])){
+            $return['status'] = 'Success';
+            $return['data'] = $res['data'];
+        }else{
+            $return['status'] = 'Fail';
+        }
+        echo json_encode($return);
+        $db->close(); 
+        die();
+    }else if($role == 'manager'){
+        $strSQL = "SELECT COUNT(noti_id) cnt FROM vot2_notification 
+              WHERE 
+              noti_view = '0' 
+              AND noti_type = 'workprocess'
+              AND nti_hcode IN (
+                  SELECT phoscode FROM vot2_projecthospital WHERE hospcode = '$hcode'
+              )
+              ";
+        $res = $db->fetch($strSQL, true, false);
+        if(($res) && ($res['status'])){
+            $return['status'] = 'Success';
+            $return['data'] = $res['data'];
+        }else{
+            $return['status'] = 'Fail';
+        }
+        echo json_encode($return);
+        $db->close(); 
+        die();
+    }else if($role == 'staff'){
+        $strSQL = "SELECT COUNT(noti_id) cnt FROM vot2_notification 
+              WHERE 
+              noti_view = '0' 
+              AND noti_type = 'workprocess'
+              AND nti_hcode = '$hcode'";
+        $res = $db->fetch($strSQL, true, false);
+        if(($res) && ($res['status'])){
+            $return['status'] = 'Success';
+            $return['data'] = $res['data'];
+        }else{
+            $return['status'] = 'Fail';
+        }
+        echo json_encode($return);
+        $db->close(); 
+        die();
+    }
+}

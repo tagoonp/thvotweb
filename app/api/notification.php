9c7494b332e9ca237366c70df1529511318d8313
Tagoon Prappre <tagoon.p@psu.ac.th>
2021-06-17 11:52:03 +0700

    20210617-10

--- app/api/notification.php
+++ app/api/notification.php
@@ -163,3 +163,89 @@ if($stage == 'read_noti'){
     $db->close(); 
     die();
 }
+
+if($stage == 'patient_noti_list'){
+    if(
+        (!isset($_GET['uid'])) ||
+        (!isset($_GET['role'])) ||
+        (!isset($_GET['page'])) ||
+        (!isset($_GET['limit']))
+    ){
+        $return['status'] = 'Fail (x101)';
+        echo json_encode($return);
+        $db->close(); 
+        die();
+    }
+
+    $uid = mysqli_real_escape_string($conn, $_GET['uid']);
+    $role = mysqli_real_escape_string($conn, $_GET['role']);
+    $page = mysqli_real_escape_string($conn, $_GET['page']);
+    $limit = mysqli_real_escape_string($conn, $_GET['limit']);
+    $page = ($page * $limit) - $limit;
+
+    $strSQL = "SELECT * FROM vot2_notification 
+              WHERE
+              noti_hide = '0' 
+              AND noti_type = 'patient_message'
+              AND noti_specific_uid = '$uid'
+              LIMIT $page, $limit
+              ";
+        $res = $db->fetch($strSQL, true, false);
+        if(($res) && ($res['status'])){
+            $return['status'] = 'Success';
+            $a = array();
+            foreach($res['data'] as $row){
+                $item = array();
+                
+                $item['noti_id'] = $row['noti_id'];
+                $item['noti_header'] = $row['noti_header'];
+                $item['noti_content'] = $row['noti_content'];
+                $item['noti_datetime'] = $row['noti_datetime'];
+                $item['noti_url'] = $row['noti_url'];
+                $item['noti_hcode'] = $row['noti_hcode'];
+                $item['noti_uid'] = $row['noti_specific_uid'];
+                $item['noti_hide'] = $row['noti_hide'];
+                
+                if($row['noti_header'] == 'แจ้งเตือนการสมัครใช้งาน'){
+                    $item['noti_redirect'] = 'userinfo';
+                    $item['noti_icon'] = 'https://thvot.com/img/register-icon.png';
+                }else{
+                    $item['noti_redirect'] = 'userinfo';
+                    $item['noti_icon'] = 'https://thvot.com/img/notification-icon.png';
+                }
+
+                $a[] = $item;
+            }
+            $return['data'] = $a;
+        }else{
+            $return['status'] = 'Fail';
+        }
+        echo json_encode($return);
+        $db->close(); 
+        die();
+}
+
+if($stage == 'patient_noti_num'){
+    $strSQL = "SELECT COUNT(noti_id) cnt FROM vot2_notification 
+              WHERE 
+              AND noti_view = '0' 
+              AND noti_type = 'patient_message'
+              AND noti_hide = '0' 
+              ";
+    $res = $db->fetch($strSQL, false);
+    if($res){
+        if($res['cnt'] != null){
+            $return['status'] = 'Success';
+            $return['data']['cn'] = $res['cnt'];
+        }else{
+            $return['status'] = 'Success';
+            $return['data']['cn'] = 0;
+        }
+    }else{
+        $return['status'] = 'Success';
+        $return['data']['cn'] = 0;
+    }
+    echo json_encode($return);
+    $db->close(); 
+    die();
+}

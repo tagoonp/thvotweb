3ca97d2437b710696e2f42dd427c7a760b48c3f4
Tagoon Prappre <tagoon.p@psu.ac.th>
2021-06-17 22:52:59 +0700

    20210617-43

--- app/api/authen-api.php
+++ app/api/authen-api.php
@@ -477,7 +477,7 @@ if($stage == 'user'){
                AND vot2_account.role = '$role'
                AND info_use = '1'
                AND (loc_status = '1' OR loc_status IS NULL)
-               AND vot2_account.delete_statys = '0'
+               AND vot2_account.delete_status = '0'
                LIMIT 1
           ";
     $user = $db->fetch($strSQL, false);

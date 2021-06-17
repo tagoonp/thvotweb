a06af2419474fac93b82d2202b1b5bbeb375abfd
Tagoon Prappre <tagoon.p@psu.ac.th>
2021-06-17 21:01:38 +0700

    20210617-34

--- app/api/create_dummy_record.php
+++ app/api/create_dummy_record.php
@@ -6,7 +6,7 @@ require('../config/database.php');
 $db = new Database();
 $conn = $db->conn();
 
-$strSQL = "SELECT * FROM vot2_account WHERE role = 'patient' AND delete_status = '0' AND end_obsdate IS NULL OR end_obsdate <= '$date'";
+$strSQL = "SELECT * FROM vot2_account WHERE role = 'patient' AND patient_type = 'VOT' AND delete_status = '0' AND end_obsdate IS NULL OR end_obsdate <= '$date'";
 $res = $db->fetch($strSQL, true, false);
 if(($res) && ($res['status'])){
     foreach ($res['data'] as $row) {

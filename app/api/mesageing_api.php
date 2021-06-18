761fd2e89b7ae30248ebd0a5e7e4f6990bdcb184
Tagoon Prappre <tagoon.p@psu.ac.th>
2021-06-18 08:44:59 +0700

    20210618-7

--- app/api/mesageing_api.php
+++ app/api/mesageing_api.php
@@ -1,21 +1,14 @@
 <?php 
-
-require('../../../database_config/thvot/config.inc.php');
-require('../config/configuration.php');
-require('../config/database.php'); 
-
-include '../../vendor/autoload.php';
-// use Hybridauth\Hybridauth;
-// use Hybridauth\HttpClient;
-
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('Nsit7f84/ZoTOjmdwgTjTn61qBGK9pESa0+63be4mMR424eDYfCljsehs1bhmh6xmBfMQdLIeTo8ccDVVkoKZYR1ifw/j70+Pnnga0as/y1AVYNuhFULeKZpaOfZJ5R1BYB/PCv65yzyUsUzYmOnigdB04t89/1O/w1cDnyilFU=');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '99ff5677d95f117fb4ad1d0cf1ff270b']);
-
-$response = $bot->replyText('U1d5a8a8fe0b1d6df97b6cef6a466ccd2', 'hello!');
-if ($response->isSucceeded()) {
-    echo 'Succeeded!';
-    return;
-}
-
-echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+curl -v -X POST https://api.line.me/v2/bot/message/multicast \
+-H 'Content-Type: application/json' \
+-H 'Authorization: Bearer {ky7UCr1R+Z02rgE4IUujkpubR5e1IOWMI72XpVGOVz94H9YbWEKfDbQnt8r9U08PbZYtSQHYT2jxFHUHNj6O5L8QgX81E4RcZ4mt8RMeruWvEDSnCwHmfHx1ocJbXshH9yPxOoWclP7b56ZGi9PgFQdB04t89/1O/w1cDnyilFU=}' \
+-d '{
+    "to": ["U1d5a8a8fe0b1d6df97b6cef6a466ccd2"],
+    "messages":[
+        {
+            "type":"text",
+            "text":"Hello, world1"
+        }
+    ]
+}'
 ?>

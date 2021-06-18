910c899e0c0d2525a701c34c98c154c07889e00f
Tagoon Prappre <tagoon.p@psu.ac.th>
2021-06-18 07:24:12 +0700

    20210618-2

--- app/api/mesageing_api.php
+++ app/api/mesageing_api.php
@@ -4,6 +4,10 @@ require('../../../database_config/thvot/config.inc.php');
 require('../config/configuration.php');
 require('../config/database.php'); 
 
+include 'vendor/autoload.php';
+// use Hybridauth\Hybridauth;
+// use Hybridauth\HttpClient;
+
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('Nsit7f84/ZoTOjmdwgTjTn61qBGK9pESa0+63be4mMR424eDYfCljsehs1bhmh6xmBfMQdLIeTo8ccDVVkoKZYR1ifw/j70+Pnnga0as/y1AVYNuhFULeKZpaOfZJ5R1BYB/PCv65yzyUsUzYmOnigdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '99ff5677d95f117fb4ad1d0cf1ff270b']);
 

935640d9222e0de590236c7f4da633940f7e91a4
Tagoon Prappre <tagoon.p@psu.ac.th>
2021-06-22 02:53:08 +0700

    20210621-39

--- app/api/patient.php
+++ app/api/patient.php
@@ -11,6 +11,47 @@ if(!isset($_GET['stage'])){ $db->close(); header('Location: ../404?stage=001'); 
 $stage = mysqli_real_escape_string($conn, $_GET['stage']);
 $return = array();
 
+if($stage == 'listofstaff'){
+    if(
+        (!isset($_GET['uid'])) ||
+        (!isset($_GET['hcode']))
+    ){
+        $return['status'] = 'Fail (x101)';
+        echo json_encode($return);
+        $db->close(); 
+        die();
+    }
+
+    $uid = mysqli_real_escape_string($conn, $_GET['uid']);
+    $hcode = mysqli_real_escape_string($conn, $_GET['hcode']);
+    $page = mysqli_real_escape_string($conn, $_GET['page']);
+    $limit = mysqli_real_escape_string($conn, $_GET['limit']);
+    $page = ($page * $limit) - $limit;
+    $strSQL = "SELECT a.uid, a.username, a.profile_img, b.fname, b.lname, a.hcode, c.hosname , a.patient_type
+                FROM vot2_account a INNER JOIN vot2_userinfo b ON a.uid = b.info_uid 
+                INNER JOIN vot2_chospital c ON a.hcode = c.hoscode 
+                WHERE 
+                a.hcode = '$hcode' 
+                AND b.info_use = '1' 
+                AND a.delete_status = '0' 
+                AND a.role = 'patient'
+                AND a.active_status = '1'
+                AND a.verify_status = '1'
+                AND a.obs_uid = '$ui'
+                LIMIT $page, $limit";
+    $res = $db->fetch($strSQL,true,false);
+    if(($res) && ($res['status'])){
+        $return['status'] = 'Success';
+        $return['data'] = $res['data'];
+    }else{
+        $return['status'] = 'Fail (x102)';
+    }
+    
+    echo json_encode($return);
+    $db->close(); 
+    die();
+}
+
 if($stage == 'list'){
     if(
         (!isset($_GET['uid'])) ||

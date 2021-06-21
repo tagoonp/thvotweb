2b238e7c471da8d20a07c0064d03984eadd1c921
Tagoon Prappre <tagoon.p@psu.ac.th>
2021-06-21 15:15:42 +0700

    20210621-9

---
+++ download_android.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Download THVOT</title>
+</head>
+<body>
+    <div style="text-align: center;">
+        <a href="market://details?id=com.thvot.vot">ดาวน์โหลดแอปพลิเคชันสำหรับผู้ป่วย</a>
+        <img src="https://thvot.com/img/patient.png" alt="" width="300">
+    </div>
+
+    <div style="text-align: center;">
+    <a href="market://details?id=com.thvot.votview">ดาวน์โหลดแอปพลิเคชันสำหรับเจ้าหน้าที่</a>
+    <img src="https://thvot.com/img/nurse.png" alt="" width="300">
+    </div>
+</body>
+</html>
---
+++ download_ios.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Download THVOT</title>
+</head>
+<body>
+    <div style="text-align: center;">
+        <a href="market://details?id=com.thvot.vot">ดาวน์โหลดแอปพลิเคชันสำหรับผู้ป่วย</a>
+    </div>
+
+    <div style="text-align: center;">
+    <a href="market://details?id=com.thvot.votview">ดาวน์โหลดแอปพลิเคชันสำหรับเจ้าหน้าที่</a>
+    </div>
+</body>
+</html>

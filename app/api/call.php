ded09d662fd7884e0a8135141011af1021c42433
Tagoon Prappre <tagoon.p@psu.ac.th>
2021-06-27 22:33:11 +0700

    202106261969

--- app/api/call.php
+++ app/api/call.php
@@ -58,7 +58,7 @@ if($stage == 'getlist'){
 
         $strSQL = "SELECT a.phone, b.fname, b.lname FROM vot2_account a INNER JOIN vot2_userinfo b ON a.uid = b.info_uid 
                     WHERE 
-                    hcode = '$hcode' AND a.delete_status = '0' AND b.info_use = '1' AND a.role = 'manage' ORDER BY a.ID DESC LIMIT 1";
+                    hcode = '$hcode' AND a.delete_status = '0' AND b.info_use = '1' AND a.role = 'manager' ORDER BY a.ID DESC LIMIT 1";
         $res4 = $db->fetch($strSQL, false);
         if($res4){
             $return['care_title'] = "พยาบาลคลินิก";

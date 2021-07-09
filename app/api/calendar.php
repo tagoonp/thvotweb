cf9448aaa593bd0a6debfbf7c73037f00e46fe17
Tagoon Prappre <tagoon.p@psu.ac.th>
2021-07-09 13:58:12 +0700

    202107091251

--- app/api/calendar.php
+++ app/api/calendar.php
@@ -23,44 +23,52 @@ if($stage == 'getpatient_calendar'){
 
     $strSQL = "SELECT start_obsdate, end_obsdate FROM vot2_account WHERE uid = '$patient_id' AND role = 'patient' AND delete_status = '0' LIMIT 1";
     $res = $db->fetch($strSQL, false);
+    
     if($res){
         $start = $res['start_obsdate'];
         $end = $res['end_obsdate'];
 
+        $start_date = strtotime($start);
+        $end_date = strtotime($end);
+
+        $date_diff = ($end_date - $start_date)/60/60/24;
+
+        echo $date_diff;
+
         
 
-        if(($start != null) && ($end != null)){
+        // if(($start != null) && ($end != null)){
             
-            do {
-                $buf = array();
+        //     do {
+        //         $buf = array();
 
-                $strSQL = "SELECT fud_status, fud_comment, fud_dateview FROM vot2_followup_dummy WHERE fud_date = '$start' AND fud_uid = '$patient_id'";
-                $res2 = $db->fetch($strSQL, false);
-                if($res2){
-                    if(($res2['fud_status'] == 'in-complete') && ($res2['fud_comment'] == null)){
-                        $buf['color'] = '#ff6246'; // ไม่ส่ง ไม่ชี้แจง 
-                    }else if(($res2['fud_status'] == 'in-complete') && ($res2['fud_comment'] != null) && ($res2['fud_comment'] != '')){
-                        $buf['color'] = '#ff6246'; // ไม่ส่ง ชี้แจง 
-                    }else if(($res2['fud_status'] == 'complete') && ($res2['fud_dateview'] == '1')){
-                        $buf['color'] = '#ff6246'; // ส่ง ได้ดู 
-                    }else if(($res2['fud_status'] == 'complete') && ($res2['fud_dateview'] == '0') && ($res2['fud_comment'] != '') && ($res2['fud_comment'] != null)){
-                        $buf['color'] = '#ff6246'; // ส่ง ชี้แจง 
-                    }else if($res2['fud_status'] == 'complete'){
-                        $buf['color'] = '#000';
-                    }
-                }else{
-                    $buf['color'] = '#000';
-                }
+        //         $strSQL = "SELECT fud_status, fud_comment, fud_dateview FROM vot2_followup_dummy WHERE fud_date = '$start' AND fud_uid = '$patient_id'";
+        //         $res2 = $db->fetch($strSQL, false);
+        //         if($res2){
+        //             if(($res2['fud_status'] == 'in-complete') && ($res2['fud_comment'] == null)){
+        //                 $buf['color'] = '#ff6246'; // ไม่ส่ง ไม่ชี้แจง 
+        //             }else if(($res2['fud_status'] == 'in-complete') && ($res2['fud_comment'] != null) && ($res2['fud_comment'] != '')){
+        //                 $buf['color'] = '#ff6246'; // ไม่ส่ง ชี้แจง 
+        //             }else if(($res2['fud_status'] == 'complete') && ($res2['fud_dateview'] == '1')){
+        //                 $buf['color'] = '#ff6246'; // ส่ง ได้ดู 
+        //             }else if(($res2['fud_status'] == 'complete') && ($res2['fud_dateview'] == '0') && ($res2['fud_comment'] != '') && ($res2['fud_comment'] != null)){
+        //                 $buf['color'] = '#ff6246'; // ส่ง ชี้แจง 
+        //             }else if($res2['fud_status'] == 'complete'){
+        //                 $buf['color'] = '#000';
+        //             }
+        //         }else{
+        //             $buf['color'] = '#000';
+        //         }
 
-                $buf['allDay'] = false;
-                $buf['start'] = $start;
+        //         $buf['allDay'] = false;
+        //         $buf['start'] = $start;
 
-                $return['status'] = 'Success';
-                $return['data'] = 
+        //         $return['status'] = 'Success';
+        //         $return['data'] = 
 
-                $start = date($start, strtotime('+1 days'));
-            }while (($start != $end) && ($start <= $date));
-        }
+        //         $start = date($start, strtotime('+1 days'));
+        //     }while (($start != $end) && ($start <= $date));
+        // }
     }else{
         echo $strSQL;
     }

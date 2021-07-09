e7f2f22681089e94155f77395ceaedbc68bfcd93
Tagoon Prappre <tagoon.p@psu.ac.th>
2021-07-09 16:47:56 +0700

    2021070 ๅ

--- app/core/assets/js/scripts/patient.js
+++ app/core/assets/js/scripts/patient.js
@@ -69,9 +69,7 @@ function getPatientCalendar(){
                   right: 'month'
                 },
                 events: [
-                    {
-                        reply
-                    }
+                    reply
                 ]
               });
             }

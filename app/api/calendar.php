caa366297403807de9e9af2e61f3ce92e41da4e3
Tagoon Prappre <tagoon.p@psu.ac.th>
2021-07-09 17:03:20 +0700

    2021070 12

--- app/api/calendar.php
+++ app/api/calendar.php
@@ -69,8 +69,7 @@ if($stage == 'getpatient_calendar'){
                     }
                 }else if($res2['fud_status'] == 'sended'){
                     if($res2['fud_dateview'] == '1'){ // ได้ดู
-                        //$buf['color'] = '#b10000'; // ส่ง ได้ดู 
-                        $buf['color'] = '#fff';
+                        $buf['color'] = '#02b869';
                     }else{ // ไม่ได้ดู
                         if($res2['fud_comment'] == null){ // ไม่ชี้แจง 
                             $buf['color'] = '#b10000'; 

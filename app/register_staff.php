2dcad248bb75d5d0824f74ba57cc7519ad5f3f04
Tagoon Prappre <tagoon.p@psu.ac.th>
2021-07-14 16:21:51 +0700

    20210714-17

--- app/register_staff.php
+++ app/register_staff.php
@@ -33,32 +33,32 @@ $photo = mysqli_real_escape_string($conn, $_GET['photo']);
     <meta name="keywords" content="admin template, Frest admin template, dashboard template, flat admin template, responsive admin template, web app">
     <meta name="author" content="PIXINVENT">
     <title>THVOT : Staff Register</title>
-    <link rel="apple-touch-icon" href="./app-assets/images/ico/apple-icon-120.png">
-    <link rel="shortcut icon" type="image/x-icon" href="./app-assets/images/ico/favicon.ico">
+    <link rel="apple-touch-icon" href="../app-assets/images/ico/apple-icon-120.png">
+    <link rel="shortcut icon" type="image/x-icon" href="../app-assets/images/ico/favicon.ico">
     <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,500,600%7CIBM+Plex+Sans:300,400,500,600,700" rel="stylesheet">
 
     <!-- BEGIN: Vendor CSS-->
-    <link rel="stylesheet" type="text/css" href="./app-assets/vendors/css/vendors.min.css">
-    <link rel="stylesheet" type="text/css" href="./app-assets/vendors/css/extensions/sweetalert2.min.css">
-    <link rel="stylesheet" type="text/css" href="./app-assets/vendors/css/forms/select/select2.min.css">
+    <link rel="stylesheet" type="text/css" href="../app-assets/vendors/css/vendors.min.css">
+    <link rel="stylesheet" type="text/css" href="../app-assets/vendors/css/extensions/sweetalert2.min.css">
+    <link rel="stylesheet" type="text/css" href="../app-assets/vendors/css/forms/select/select2.min.css">
     <!-- END: Vendor CSS-->
 
     <!-- BEGIN: Theme CSS-->
-    <link rel="stylesheet" type="text/css" href="./app-assets/css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="./app-assets/css/bootstrap-extended.css">
-    <link rel="stylesheet" type="text/css" href="./app-assets/css/colors.css">
-    <link rel="stylesheet" type="text/css" href="./app-assets/css/components.css">
-    <link rel="stylesheet" type="text/css" href="./app-assets/css/themes/dark-layout.css">
-    <link rel="stylesheet" type="text/css" href="./app-assets/css/themes/semi-dark-layout.css">
+    <link rel="stylesheet" type="text/css" href="../app-assets/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="../app-assets/css/bootstrap-extended.css">
+    <link rel="stylesheet" type="text/css" href="../app-assets/css/colors.css">
+    <link rel="stylesheet" type="text/css" href="../app-assets/css/components.css">
+    <link rel="stylesheet" type="text/css" href="../app-assets/css/themes/dark-layout.css">
+    <link rel="stylesheet" type="text/css" href="../app-assets/css/themes/semi-dark-layout.css">
     <!-- END: Theme CSS-->
 
     <!-- BEGIN: Page CSS-->
-    <link rel="stylesheet" type="text/css" href="./app-assets/css/core/menu/menu-types/vertical-menu.css">
-    <link rel="stylesheet" type="text/css" href="./app-assets/css/pages/authentication.css">
+    <link rel="stylesheet" type="text/css" href="../app-assets/css/core/menu/menu-types/vertical-menu.css">
+    <link rel="stylesheet" type="text/css" href="../app-assets/css/pages/authentication.css">
     <!-- END: Page CSS-->
 
     <!-- BEGIN: Custom CSS-->
-    <link rel="stylesheet" type="text/css" href="./assets/css/style.css">
+    <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
     <!-- END: Custom CSS-->
 
 </head>
@@ -183,7 +183,7 @@ $photo = mysqli_real_escape_string($conn, $_GET['photo']);
                                 </div>
                                 <!-- image section right -->
                                 <div class="col-md-6 d-md-block d-none text-center align-self-center p-3">
-                                    <img class="img-fluid" src="./app-assets/images/pages/register.png" alt="branding logo">
+                                    <img class="img-fluid" src="../app-assets/images/pages/register.png" alt="branding logo">
                                 </div>
                             </div>
                         </div>
@@ -198,28 +198,28 @@ $photo = mysqli_real_escape_string($conn, $_GET['photo']);
 
 
     <!-- BEGIN: Vendor JS-->
-    <script src="./app-assets/vendors/js/vendors.min.js"></script>
-    <script src="./app-assets/fonts/LivIconsEvo/js/LivIconsEvo.tools.js"></script>
-    <script src="./app-assets/fonts/LivIconsEvo/js/LivIconsEvo.defaults.js"></script>
-    <script src="./app-assets/fonts/LivIconsEvo/js/LivIconsEvo.min.js"></script>
-    <script src="./app-assets/vendors/js/extensions/sweetalert2.all.min.js"></script>
-    <script src="./app-assets/vendors/js/extensions/polyfill.min.js"></script>
-    <script src="./app-assets/vendors/js/forms/select/select2.full.min.js"></script>
+    <script src="../app-assets/vendors/js/vendors.min.js"></script>
+    <script src="../app-assets/fonts/LivIconsEvo/js/LivIconsEvo.tools.js"></script>
+    <script src="../app-assets/fonts/LivIconsEvo/js/LivIconsEvo.defaults.js"></script>
+    <script src="../app-assets/fonts/LivIconsEvo/js/LivIconsEvo.min.js"></script>
+    <script src="../app-assets/vendors/js/extensions/sweetalert2.all.min.js"></script>
+    <script src="../app-assets/vendors/js/extensions/polyfill.min.js"></script>
+    <script src="../app-assets/vendors/js/forms/select/select2.full.min.js"></script>
     <!-- BEGIN Vendor JS-->
 
     <!-- BEGIN: Page Vendor JS-->
     <!-- END: Page Vendor JS-->
 
     <!-- BEGIN: Theme JS-->
-    <script src="./app-assets/js/scripts/configs/vertical-menu-light.js"></script>
-    <script src="./app-assets/js/core/app-menu.js"></script>
-    <script src="./app-assets/js/core/app.js"></script>
-    <script src="./app-assets/js/scripts/components.js"></script>
-    <script src="./app-assets/js/scripts/footer.js"></script>
+    <script src="../app-assets/js/scripts/configs/vertical-menu-light.js"></script>
+    <script src="../app-assets/js/core/app-menu.js"></script>
+    <script src="../app-assets/js/core/app.js"></script>
+    <script src="../app-assets/js/scripts/components.js"></script>
+    <script src="../app-assets/js/scripts/footer.js"></script>
     <!-- END: Theme JS-->
 
     <!-- BEGIN: Page JS-->
-    <script src="./assets/js/scripts/authen.js?v=<?php echo filemtime('./assets/js/scripts/authen.js'); ?>"></script>
+    <script src="../assets/js/scripts/authen.js?v=<?php echo filemtime('./assets/js/scripts/authen.js'); ?>"></script>
     <!-- END: Page JS-->
 
     <script>

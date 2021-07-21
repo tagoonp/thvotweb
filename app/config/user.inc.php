f4b925c8f99e23bf4d0d2a17b8c0a6f4a4bdc18a
Tagoon Prappre <tagoon.p@psu.ac.th>
2021-07-21 21:57:10 +0700

    20210721-3

--- app/config/user.inc.php
+++ app/config/user.inc.php
@@ -15,10 +15,18 @@ if($_SESSION['thvot_role'] != $active_role){
     header('Location: '.ROOT_DOMAIN);   
 }
 
+// $strSQL = "SELECT * FROM vot2_account INNER JOIN vot2_chospital ON vot2_account.hcode = vot2_chospital.hoscode 
+//             INNER JOIN vot2_userinfo ON vot2_account.uid = vot2_userinfo.info_uid
+//           WHERE 
+//           vot2_account.UID = '".$_SESSION['thvot_uid']."' 
+//           AND vot2_account.role = '".$_SESSION['thvot_role']."'
+//           AND info_use = '1'
+//           ";
+
 $strSQL = "SELECT * FROM vot2_account INNER JOIN vot2_chospital ON vot2_account.hcode = vot2_chospital.hoscode 
             INNER JOIN vot2_userinfo ON vot2_account.uid = vot2_userinfo.info_uid
           WHERE 
-          vot2_account.UID = '".$_SESSION['thvot_uid']."' AND vot2_account.role = '".$_SESSION['thvot_role']."'
+          vot2_account.UID = '".$_SESSION['thvot_uid']."' 
           AND info_use = '1'
           ";
 $user = $db->fetch($strSQL, false);

075759a9da6f95f7a0b6d7ffce0769718435191f
Tagoon Prappre <tagoon.p@psu.ac.th>
2021-07-22 22:34:23 +0700

    20210722-9

--- app/core/system/web/admin/control/profile_menu.php
+++ app/core/system/web/admin/control/profile_menu.php
@@ -1,12 +1,12 @@
 <li class="dropdown dropdown-user nav-item"><a class="dropdown-toggle nav-link dropdown-user-link" href="javascript:void(0);" data-toggle="dropdown">
-        <div class="user-nav d-sm-flex d-none"><span class="user-name"><?php echo $user['fname']." ".$user['lname']; ?> <span class="badge badge-danger">(ผู้ดูแลระบบ)</span></span><span class="user-status text-muted">
-        <?php 
-                                                            if($user['role'] == 'administrator'){ echo "ผู้ดูแลระบบ"; }
-                                                            if($user['role'] == 'moderator'){ echo "ผู้รับผิดชอบส่วนกลาง"; }
-                                                            if($user['role'] == 'manager'){ echo "ผู้รับผิดชอบส่วนงานสถานบริการ"; }
-                                                            if($user['role'] == 'staff'){ echo "ผู้ปฏิบัติงานบันทึกข้อมูล"; }
-                                                            if($user['role'] == 'patient'){ echo "ผู้ป่วย"; }
-                                                            ?>
+        <div class="user-nav d-sm-flex d-none"><span class="user-name"><?php echo $user['fname']." ".$user['lname']; ?> <span class="badge badge-danger">(<?php 
+        if($user['role'] == 'administrator'){ echo "ผู้ดูแลระบบ"; }
+        if($user['role'] == 'moderator'){ echo "ผู้รับผิดชอบส่วนกลาง"; }
+        if($user['role'] == 'manager'){ echo "ผู้รับผิดชอบส่วนงานสถานบริการ"; }
+        if($user['role'] == 'staff'){ echo "ผู้ปฏิบัติงานบันทึกข้อมูล"; }
+        if($user['role'] == 'patient'){ echo "ผู้ป่วย"; }
+        ?>)</span></span><span class="user-status text-muted">
+        
         </span></div><span><img class="round" src="<?php if(($user['profile_img'] != '') && ($user['profile_img'] != null)){ echo $user['profile_img']; }else{ echo "../../../app-assets/images/portrait/small/avatar-s-11.jpg"; }?>" alt="avatar" height="40" width="40"></span>
     </a>
     <div class="dropdown-menu dropdown-menu-right pb-0">

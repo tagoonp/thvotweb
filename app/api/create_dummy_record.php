911a89f31e171daa9fbdb3077fcb26f31eb6e15e
Tagoon Prappre <tagoon.p@psu.ac.th>
2021-07-29 21:35:55 +0700

    20210729-3

--- app/api/create_dummy_record.php
+++ app/api/create_dummy_record.php
@@ -11,7 +11,7 @@ $strSQL = "SELECT * FROM vot2_account
            role = 'patient' 
            AND patient_type = 'VOT' 
            AND delete_status = '0' 
-           AND (end_obsdate IS NULL OR end_obsdate >= '$date')
+           AND (cal_end_obsdate IS NULL OR cal_end_obsdate >= '$date')
            AND uid NOT IN (SELECT fud_uid FROM vot2_followup_dummy WHERE fud_date = '$date')
            ";
 $res = $db->fetch($strSQL, true, false);
@@ -19,7 +19,8 @@ if(($res) && ($res['status'])){
     foreach($res['data'] as $row) {
         $strSQL = "INSERT INTO vot2_followup_dummy (`fud_uid`, `fud_username`, `fud_status`, `fud_date`, `fud_followstage`)
                    VALUES ('".$row['uid']."', '".$row['username']."', 'non-response', '$date', '1')";
-        if($row['end_obsdate'] != $row['cal_end_obsdate']){
+        // if($row['end_obsdate'] != $row['cal_end_obsdate']){
+        if($row['stop_drug'] == 1){
             $strSQL = "INSERT INTO vot2_followup_dummy (`fud_uid`, `fud_username`, `fud_status`, `fud_date`, `fud_followstage`)
                        VALUES ('".$row['uid']."', '".$row['username']."', 'non-response', '$date', '0') ";
         }

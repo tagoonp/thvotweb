d8d5c958d9e0ffaba82e82c6d88a06315ccf66b7
Tagoon Prappre <tagoon.p@psu.ac.th>
2021-08-24 15:17:29 +0700

    20210824-19

--- app/core/system/web/patient/control/admin-menu.php
+++ app/core/system/web/patient/control/admin-menu.php
@@ -1,6 +1,6 @@
 <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation" data-icon-style="lines">
                 <li class="nav-item <?php if($menu == 0){ echo "active"; } ?>"><a href="./"><i class="menu-livicon" data-icon="desktop"></i><span class="menu-title text-truncate">หน้าแรก</span></a></li>
-                <li class="nav-item <?php if($menu == 1){ echo "active"; } ?>"><a href="./"><i class="menu-livicon" data-icon="alarm"></i><span class="menu-title text-truncate">ตั้งเวลา</span></a></li>
-                <li class="nav-item <?php if($menu == 2){ echo "active"; } ?>"><a href="./"><i class="menu-livicon" data-icon="users"></i><span class="menu-title text-truncate">พี่เลี้ยง</span></a></li>
+                <li class="nav-item <?php if($menu == 1){ echo "active"; } ?>"><a href="./alarm"><i class="menu-livicon" data-icon="alarm"></i><span class="menu-title text-truncate">ตั้งเวลา</span></a></li>
+                <li class="nav-item <?php if($menu == 2){ echo "active"; } ?>"><a href="./observer"><i class="menu-livicon" data-icon="users"></i><span class="menu-title text-truncate">พี่เลี้ยง</span></a></li>
                 
             </ul>

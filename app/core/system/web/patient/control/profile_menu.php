b35ce9f4d306c2c295097b495462c04daf2d3fd8
Tagoon Prappre <tagoon.p@psu.ac.th>
2021-08-26 15:17:15 +0700

    20210826-2

--- app/core/system/web/patient/control/profile_menu.php
+++ app/core/system/web/patient/control/profile_menu.php
@@ -1,13 +1,23 @@
 <li class="dropdown dropdown-user nav-item"><a class="dropdown-toggle nav-link dropdown-user-link" href="javascript:void(0);" data-toggle="dropdown">
         <div class="user-nav d-sm-flex d-none"><span class="user-name"><?php echo $user['fname']." ".$user['lname']; ?> <span class="badge badge-danger">(<?php 
-        if($user['role'] == 'admin'){ echo "ผู้ดูแลระบบ"; }
-        if($user['role'] == 'moderator'){ echo "ผู้รับผิดชอบส่วนกลาง"; }
-        if($user['role'] == 'manager'){ echo "พยาบาลคลินิก"; }
-        if($user['role'] == 'staff'){ echo "พี่เลี้ยง"; }
-        if($user['role'] == 'patient'){ echo "ผู้ป่วย"; }
-        ?>)</span></span><span class="user-status text-muted">
-        
-        </span></div><span><img class="round" src="<?php if(($user['profile_img'] != '') && ($user['profile_img'] != null)){ echo $user['profile_img']; }else{ echo "../../../app-assets/images/portrait/small/avatar-s-11.jpg"; }?>" alt="avatar" height="40" width="40"></span>
+            if($user['role'] == 'admin'){ echo "ผู้ดูแลระบบ"; }
+            if($user['role'] == 'moderator'){ echo "ผู้รับผิดชอบส่วนกลาง"; }
+            if($user['role'] == 'manager'){ echo "พยาบาลคลินิก"; }
+            if($user['role'] == 'staff'){ echo "พี่เลี้ยง"; }
+            if($user['role'] == 'patient'){ echo "ผู้ป่วย"; }
+            ?>)</span></span>
+            <!-- <span class="user-status text-muted">
+            
+            </span> -->
+        </div>
+        <?php 
+            if($menu != 99){
+                ?>
+                <span><img class="round" src="<?php if(($user['profile_img'] != '') && ($user['profile_img'] != null)){ echo $user['profile_img']; }else{ echo "../../../app-assets/images/portrait/small/avatar-s-11.jpg"; }?>" alt="avatar" height="40" width="40"></span>    
+                <?php
+            }
+        ?>
+            
     </a>
     <div class="dropdown-menu dropdown-menu-right pb-0">
         <a class="dropdown-item" href="user-profile"><i class="bx bx-user mr-50"></i> ข้อมูลส่วนตัว</a>

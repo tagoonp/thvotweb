cb2db5f446991369e4b4a6d1ffaaedefd0ceb662
Tagoon Prappre <tagoon.p@psu.ac.th>
2021-09-12 23:48:12 +0700

    20210912-7

--- app/core/app-assets/js/scripts/pages/app-users.js
+++ app/core/app-assets/js/scripts/pages/app-users.js
@@ -21,7 +21,7 @@ $(document).ready(function () {
             'columnDefs': [
                 {
                     "orderable": false,
-                    "targets": [7]
+                    "targets": [6]
                 }]
         });
     };

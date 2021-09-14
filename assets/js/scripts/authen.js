4819eb198bbb8ff1f85db9164d8bd97a92e48154
Tagoon Prappre <tagoon.p@psu.ac.th>
2021-09-14 09:15:50 +0700

    20210913-18

--- assets/js/scripts/authen.js
+++ assets/js/scripts/authen.js
@@ -129,9 +129,9 @@ var auth = {
       $check++; $('#txtHn').addClass('is-invalid')
     }
 
-    if($('#txtUsername').val() == ''){
-      $check++; $('#txtUsername').addClass('is-invalid')
-    }
+    // if($('#txtUsername').val() == ''){
+    //   $check++; $('#txtUsername').addClass('is-invalid')
+    // }
   
     if($('#txtPhone').val() == ''){
       $check++; $('#txtPhone').addClass('is-invalid')
